43793eb25dd2afed88820fd8f68ced30c267ab2c
RajuKoushik <g.rajukoushik@gmail.com>
2016-09-02 16:14:01 +0530

    Updated models.py .

--- td/admin.py
+++ td/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User,TravelDiary, Post , TravelConnection
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(TravelDiary)
+admin.site.register(Post)
+admin.site.register(TravelConnection)
---
+++ td/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class User(models.Model):
+    user_id = models.IntegerField()
+    nick_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.PositiveIntegerField(default=0)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    join_date = models.DateTimeField('Join published')
+
+
+
+    def __str__(self):
+        return self.nick_name
+
+        # adding a custom database
+
+
+class TravelDiary(models.Model):
+    loc_id = models.IntegerField()
+    loc_name = models.CharField(max_length=30)
+    loc_text = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.choice_text
+
+class Post(models.Model):
+    post_id = models.IntegerField()
+    post_title = models.CharField(max_length=30)
+    post_text = models.CharField(max_length=200)
+
+    post_votes = models.IntegerField()
+
+    def __str__(self):
+        return self.post_title
+
+class TravelConnection(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    loc_id = models.ForeignKey(TravelDiary, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_id
+
+
+
+
+
+
