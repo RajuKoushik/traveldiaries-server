c563bcfc404ae38e611f75c6b4935842b1fc2281
rajukoushik <g.rajukoushik@gmail.com>
2016-11-02 16:25:43 +0530

    Updaed the Signup and Login api.

---
+++ td/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from rest_framework.authtoken import views as rest_views
+from . import views
+
+urlpatterns = [
+    url(r'^login/', rest_views.obtain_auth_token),
+    url(r'^signup/?', views.post_sign_up)
+]
--- td/views.py
+++ td/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import json
+
+from django.db import transaction
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime, timedelta
+
+from django.http import HttpResponse
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+from . import models
+
+@csrf_exempt
+def post_sign_up(request):
+    with transaction.atomic():
+        user = User.objects.create_user(
+            username=request.POST.get('username'),
+            password=request.POST.get('password'),
+            email=request.POST.get('email')
+        )
+        user.save()
+        user.first_name = request.POST.get('first_name', '')
+        user.last_name = request.POST.get('last_name', '')
+        userinfo = models.UserInfo(user=user)
+        userinfo.save()
+
+    return HttpResponse(
+        json.dumps(
+            {
+                'token': Token.objects.get(user=user).key
+            }
+        )
+    )
+
